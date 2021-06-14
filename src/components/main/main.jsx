fa584590abb79fb042b7d9e887a3892e2e81f561
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-06-14 18:38:10 +0300

    Добалена история конвертации

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Promo from '../promo/promo';
 import Form from '../form/form';
+import ExchangeHistory from '../exchange-history/exchange-history';
 
 const Main = () => {
   return (
     <main>
       <Promo/>
       <Form/>
+      <ExchangeHistory/>
     </main>
   );
 };
